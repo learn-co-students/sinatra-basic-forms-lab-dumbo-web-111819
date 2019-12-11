f4e76236441847e20daa9e2dbe39a8f589bb24b4
“Saima <rahman.saima92@gmail.com>
2019-12-11 16:32:42 -0500

    Done.

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
 class Puppy
-  attr_reader :breed
+  attr_accessor :breed
   attr_accessor :age, :name
 
   def initialize(params)
     @name = params[:name]
-    @breed =params[:breed]
+    @breed = params[:breed]
     @age = params[:age]
   end
 end
