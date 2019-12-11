1312317a04d7c7d17b43d67783f74be63f66d9bd
“Saima <rahman.saima92@gmail.com>
2019-12-11 16:29:53 -0500

    Done.

--- app.rb
+++ app.rb
@@ -11,9 +11,9 @@ class App < Sinatra::Base
 
   post '/puppy' do
     # binding.pry
-    # @puppy = Puppy.new(params)
+    @puppy = Puppy.new(params)
     # binding.pry
-     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+  #? @puppy = Puppy.new(params[:name], params[:breed], params[:age])
 
     erb :display_puppy
   end
--- models/puppy.rb
+++ models/puppy.rb
@@ -2,9 +2,9 @@ class Puppy
   attr_reader :breed
   attr_accessor :age, :name
 
-  def initialize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
+  def initialize(params)
+    @name = params[:name]
+    @breed =params[:breed]
+    @age = params[:age]
   end
 end
